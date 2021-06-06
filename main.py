253ce3b88ccd8670bcb82a76f40c11499f9fb452
allen maxwell <allen.maxwell@outlook.com>
2021-06-06 16:30:24 -0700

    Initial commit

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from passlib.hash import pbkdf2_sha256
 from model import Task, User
 
 app = Flask(__name__)
-app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+# app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
 app.secret_key = os.environ.get('SECRET_KEY').encode()
-# getting a random secret key from an environment variable. 
-# app.secret_key = os.environ.get('FLASK_SESSION_KEY', os.urandom(24).hex())
 
 @app.route('/all')
 def all_tasks():
